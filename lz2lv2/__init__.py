607d77bbdf385708fadf3d6382d7a0e25d4aefd8
Danilo de Jesus da Silva Bellini <danilo.bellini@gmail.com>
2014-09-25 07:08:10 -0300

    Package __init__.py module with a few metadata

---
+++ lz2lv2/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created on Fri 2014-09-19 18:56:48 BRT
+# License is GPLv3, see COPYING.txt for more details.
+# @author: Danilo de Jesus da Silva Bellini
+"""
+AudioLazy to LV2!
+"""
+
+__version__ = "0.1dev"
+__author__ = "Danilo de Jesus da Silva Bellini"
+__author_email__  = "danilo [dot] bellini [at] gmail [dot] com"
+__url__ = "http://github.com/danilobellini/lz2lv2"
